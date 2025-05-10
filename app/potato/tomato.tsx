aac38b44d7d5f2e6a673d164e615b33a2346d400
jiho <ghrms80@gmail.com>
2025-05-10 18:07:14 +0900

    1.3 Layout Component

---
+++ app/potato/tomato.tsx
@@ -0,0 +1,12 @@
+export default function Tomato() {
+  return <div>Tomato</div>;
+}
+
+export const links = () => [
+  { rel: "stylesheet", href: "https://potato.com" },
+];
+
+export const meta = () => [
+  { title: "About Us" },
+  { description: "About Us" },
+];
