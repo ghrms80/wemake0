8c6383ac2a2faf3a4ad9ccd1e0be48c8bc10daf1
jiho <ghrms80@gmail.com>
2025-05-29 08:37:42 +0900

    3.15 Data Validation

--- app/features/products/pages/daily-leaderboard-page.tsx
+++ app/features/products/pages/daily-leaderboard-page.tsx
@@ -1,7 +1,84 @@
-export default function DailyLeaderboardPage() {
+import { DateTime } from "luxon";
+import type { Route } from "./+types/daily-leaderboard-page";
+import { data, isRouteErrorResponse } from "react-router";
+import { z } from "zod";
+
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+  day: z.coerce.number(),
+});
+
+export const loader = ({ params }: Route.LoaderArgs) => {
+  // const { year, month, day } = params;
+  const { success, data:parsedData } = paramsSchema.safeParse(params);
+
+  if (!success)
+  {
+    throw data(
+      {
+        error_code: "invalid_params",
+        error_message: "Invalid params",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const date = DateTime.fromObject(parsedData).setZone("Asia/Seoul");
+
+  if (!date.isValid) {
+    // throw new Error("Invalid date");
+    throw data(
+      {
+        error_code: "invalid_date",
+        error_message: "Invalid date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const today = DateTime.now().setZone("Asia/Seoul").startOf("day");
+  if (date > today){
+    throw data(
+      {
+        error_code: "future_date",
+        error_message: "Future date",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  return {
+    date,
+  }
+}
+
+export default function DailyLeaderboardPage({ loaderData }: Route.ComponentProps) {
   return (
     <div>
       <h1>Daily Leaderboard Page</h1>
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.error_message} / {error.data.error_code}
+      </div>
+    );
+  }
+
+  if (error instanceof Error) {
+    return <div>{error.message}</div>
+  }
+
+  return <div>Unknown error</div>
 }
